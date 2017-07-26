306b1caa5ce31141117d90365fc0d5a4d6fbc4fb
Iku Iwasa <iku.iwasa@gmail.com>
2017-07-26 22:57:54 +0900

    Debug ios build failure

--- ci/run-ios.sh
+++ ci/run-ios.sh
@@ -14,10 +14,11 @@ case "$TARGET" in
     curl -vv -L https://github.com/carllerche/ios-test-harness/releases/download/v0.1.0/libiosharness-$TARGET.a > libiosharness.a;
 
     # Build the test
-    cargo rustc --test test --target $TARGET -- \
+    cargo rustc -v --test test --target $TARGET -- \
         -L . \
         -C link-args="-mios-simulator-version-min=7.0 -e _ios_main -liosharness";
 
+    find x86_64-apple-ios
 
     # Find the file to run
     TEST_FILE="$(find target/$TARGET/debug -maxdepth 1 -type f -name test-* | head -1)";
